fc37d7e7975c29cefd82356bcc887cf2dc916da9
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2022-06-20 21:21:44 +0200

    Initial commit; Empty app.

---
+++ app/src/main/java/net/ssh_pi_control/sshpicontrol/MainActivity.java
@@ -0,0 +1,14 @@
+package net.ssh_pi_control.sshpicontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
