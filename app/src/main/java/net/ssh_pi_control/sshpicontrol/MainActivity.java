be9756f34bae9e390d7d694ea74449266f8e0c6a
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2022-06-20 21:29:28 +0200

    SSH command function added to MainActivity.java. Basic functionality working.

--- app/src/main/java/net/ssh_pi_control/sshpicontrol/MainActivity.java
+++ app/src/main/java/net/ssh_pi_control/sshpicontrol/MainActivity.java
@@ -1,8 +1,17 @@
 package net.ssh_pi_control.sshpicontrol;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void doIt (View view) {
+        new AsyncTask<Integer, Void, Void>() {
+            @Override
+            protected Void doInBackground(Integer... params) {
+                try {
+                    executeRemoteCommand("pi", "","192.168.8.105", 22);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(1);
+    }
+
+    public static String executeRemoteCommand(String username,String password,String hostname,int port)
+            throws Exception {
+
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(username, hostname, port);
+        session.setPassword(password);
+
+        Properties prop = new Properties();
+        prop.put("StrictHostKeyChecking", "no");
+        session.setConfig(prop);
+
+        session.connect();
+
+        ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        channelssh.setOutputStream(baos);
+
+        channelssh.setCommand("touch /home/pi/hello_moana");
+        channelssh.connect();
+        channelssh.disconnect();
+
+        return baos.toString();
     }
 }
